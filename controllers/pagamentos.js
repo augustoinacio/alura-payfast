3018c4c8a88bc30727e5a90a34f4f3dc5a3ec8fa
Augusto Inacio <augustoinacio@gmail.com>
2017-11-27 00:35:13 -0200

    capitulos 1 e 2

---
+++ controllers/pagamentos.js
@@ -0,0 +1,10 @@
+module.exports = function (app) {
+    app.get('/pagamentos', function(req, res){
+        res.send('OK.');
+    });
+    app.post('/pagamentos/pagamento', function(req,res){
+        var pagamento = req.body;
+        console.log(pagamento);
+        res.send('OK.')
+    })
+}
